ef336f3b880124ca12e4595d349370d4b7576e75
Gina Fedder <ginafedder@yahoo.com>
2018-08-28 18:27:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,17 @@
 var playlist = {artistName: "songTitle"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
+<<<<<<< HEAD
   playlist[artistName] = 'songTitle';
 }
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
+=======
+  return Object.assign({}, playlist, {['artistName']: 'songTitle'});
+}
+
+function removeFromPlaylist(playlist, artistNames) {
+  return delete playlist.artistName;
+>>>>>>> 4f985bc300c5e4bbb582169b4fae7f789c94ce6e
 }
